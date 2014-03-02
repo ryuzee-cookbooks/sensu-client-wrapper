5f7ece25c2c81e7d908752d189a08f6a6899cac6
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-03-02 18:19:52 +0900

    add condition

--- recipes/auto-remove.rb
+++ recipes/auto-remove.rb
@@ -5,24 +5,26 @@ else
 end
 node_name.gsub!(/[^A-z0-9]+/,".")
 
-template "/etc/rc.d/init.d/sensu-client-remove" do
-  source "sensu-client-remove.erb"
-  mode 0755
-  owner "root"
-  group "root"
-  variables({
-    :node_name => node_name
-  })
-end
-
-execute "/sbin/chkconfig --level 0 sensu-client-remove off" do
-  action :run
-end
-
-execute "/sbin/chkconfig --level 2345 sensu-client-remove on" do
-  action :run
-end
-
-execute "/etc/rc.d/init.d/sensu-client-remove start" do
-  action :run
+if platform_family?("rhel")
+  template "/etc/rc.d/init.d/sensu-client-remove" do
+    source "sensu-client-remove.erb"
+    mode 0755
+    owner "root"
+    group "root"
+    variables({
+      :node_name => node_name
+    })
+  end
+  
+  execute "/sbin/chkconfig --level 0 sensu-client-remove off" do
+    action :run
+  end
+  
+  execute "/sbin/chkconfig --level 2345 sensu-client-remove on" do
+    action :run
+  end
+  
+  execute "/etc/rc.d/init.d/sensu-client-remove start" do
+    action :run
+  end
 end
