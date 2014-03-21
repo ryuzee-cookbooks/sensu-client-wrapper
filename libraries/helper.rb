8bded0a60fca1af06a29c926249890a5330e991b
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-03-21 17:38:27 +0900

    use Chef::Config[:node_name] as default sensu node name

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -4,7 +4,7 @@ class Chef
       if @node["sensu-client-wrapper"]["name"] then
         node_name = @node["sensu-client-wrapper"]["name"]
       else
-        node_name = @node["hostname"]
+        node_name = Chef::Config[:node_name]
       end
       node_name.gsub!(/[^\w\.\-]+/,".")
       return node_name
