1534c2f3feb539f34b29f5396b31a530d6aef00b
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-10-05 09:50:27 +0900

    fix for serverspec 2.0

--- test/integration/default/serverspec/localhost/sensu_spec.rb
+++ test/integration/default/serverspec/localhost/sensu_spec.rb
@@ -1,13 +1,4 @@
-require 'serverspec'
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.os = backend(Serverspec::Commands::Base).check_os
-  end
-  c.path = "/sbin:/usr/sbin"
-end
+require 'spec_helper'
 
 %w{sensu}.each do |p|
   describe package(p) do
