ddaee7ba52f551690bf796f021f012202ed2ebd8
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-04-16 18:43:44 +0900

    fix node_name

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -1,11 +1,7 @@
 class Chef
   class Recipe
     def node_name
-      begin
-        node_name = @node["sensu-client-wrapper"]["name"]
-      rescue
-        node_name = Chef::Config[:node_name] || @node[:hostname]
-      end
+      node_name = node["sensu-client-wrapper"]["name"] || Chef::Config[:node_name] || node[:hostname]
       node_name.gsub!(/[^\w\.\-]+/,".")
       return node_name
     end
